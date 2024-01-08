e2a812c3c9712d982fe004e447cb270412dd2ea3
KHIRAJ UMREDKAR <kdumredkar@gmail.com>
2024-01-08 12:25:48 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Khiraj Umredkar on 08/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
